83207ed41ac3697b6fef1b19385658135ebf3f38
zsl <345154004@qq.com>
2016-11-03 11:32:20 +0800

    add element-ui

--- resources/assets/js/home/App.vue
+++ resources/assets/js/home/App.vue
@@ -1,15 +1,22 @@
 <template>
     <div class="wrapper" id="app">
-        <click></click>
+        <el-row :gutter="20">
+            <el-col :span="6">
+                <el-button icon="edit">Hello Element</el-button>
+            </el-col>
+            <el-col :span="18">
+                <click></click>
+            </el-col>
+        </el-row>
     </div>
 </template>
 <script>
-    import Click from './components/Click.vue';
+    import Click from './components/Click.vue'
 
   export default {
     name: 'app',
     components: {
       Click
     }
-  };
+  }
 </script>
--- resources/assets/js/home/app.js
+++ resources/assets/js/home/app.js
@@ -1,13 +1,16 @@
-import Vue from 'vue';
-import App from './App.vue';
-import VueResource from 'vue-resource';
-import VueRouter from 'vue-router';
+import Vue from 'vue'
+import App from './App.vue'
+import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
 
-Vue.use(VueResource);
-Vue.http.headers.common['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]').content;
+Vue.use(VueResource)
+Vue.http.headers.common['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]').content
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
+Vue.use(ElementUI)
 
 const router = new VueRouter({
 	hashbang: false,
@@ -21,4 +24,4 @@ const app = new Vue({
   store,
   router,
   render: h => h(App)
-});
+})
