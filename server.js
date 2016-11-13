0f7e51658fb04a295e1028156b26d467bb5332c5
ko22009 <ko22009@yandex.ru>
2016-11-14 02:39:46 +0500

    fix problem with output path and bootstrap-sass in gulp

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express()
 const compiler = webpack(WebpackConfig)
 
 app.use(webpackDevMiddleware(compiler, {
-  publicPath: '/js',
+  publicPath: '/js/',
   stats: {
     colors: true,
     chunks: false
